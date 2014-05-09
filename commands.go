807400db6a33b6102743636b4fc4493bada3407f
Chris Johnson <chris@cjohnson.io>
2014-05-08 19:48:42 -0700

    fixed stuff

--- commands.go
+++ commands.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func listPosts() {
 	pln("Listing your drafts...")
 
@@ -8,14 +13,16 @@ func listPosts() {
 	} else {
 		pln("No drafts yet!")
 	}
+
+	sync()
 }
 
-func addPost(shortname string) error {
+func addPost(shortname string) {
 	pln("Adding a post!")
 
 	err, _ := manifest.Add(shortname)
 	if err != nil {
-		return err
+		logxit(err)
 	} else {
 		pln("Successfully added the item!")
 		pln("Now saving the manifest to S3...")
@@ -28,6 +35,33 @@ func addPost(shortname string) error {
 
 		exit("Successfully saved!")
 	}
+}
 
-	return nil
+func sync() {
+	if DRAFT_DIR == "" {
+		exit("You need to set your $DRAFT_DIR variable to point to your drafts folder")
+	}
+
+	draftDir, err := os.Open(DRAFT_DIR) // For read access.
+	if err != nil {
+		logxit(err)
+	}
+
+	files, err := draftDir.Readdir(0)
+	if err != nil {
+		logxit(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			// Do nothing
+		} else {
+
+			pln("name = " + file.Name())
+		}
+	}
+
+	pln("names = !")
+	fmt.Printf("how many files? = %v", len(files))
+	// ps(names)
 }
